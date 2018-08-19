872df488b542a556f0f90bf75094c73aeab5aa37
Amaury Martiny <amaurymartiny@Amaurys-MacBook-Pro.local>
2018-08-19 23:25:52 +0200

    Use tsx for _document

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -1,12 +1,18 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, {
+  Head,
+  Main,
+  NextDocumentContext,
+  NextScript
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps({ renderPage }: NextDocumentContext) {
     // Make NextJs work with styled components
     // https://github.com/zeit/next.js/tree/canary/examples/with-styled-components
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
+    // TODO Find a better type for App
+    const page = renderPage((App: any) => (props: object) =>
       sheet.collectStyles(<App {...props} />)
     );
     const styleTags = sheet.getStyleElement();
