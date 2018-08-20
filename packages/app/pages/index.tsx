719186ad17fdb937dc4851134b4b54f6046f33b9
Amaury Martiny <amaury.martiny@protonmail.com>
2018-08-20 10:38:42 +0200

    Correctly convert dates

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -10,10 +10,7 @@ class Index extends React.Component {
     const [today] = new Date().toISOString().split('T');
 
     // `today` is in dd-mm-yyyy format, we convert it to yyyy/mm/dd format
-    const newUrl = today
-      .split('-')
-      .reverse()
-      .join('/');
+    const newUrl = today.split('-').join('/');
 
     // https://github.com/zeit/next.js/wiki/Redirecting-in-%60getInitialProps%60
     if (res) {
