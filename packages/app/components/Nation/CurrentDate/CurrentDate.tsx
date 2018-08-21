1aab0aa3a18af8a95de89d6ea07aba6524712fe5
Amaury Martiny <amaury.martiny@protonmail.com>
2018-08-21 14:41:10 +0200

    Allow changing date manually

--- packages/app/components/Nation/CurrentDate/CurrentDate.tsx
+++ packages/app/components/Nation/CurrentDate/CurrentDate.tsx
@@ -6,30 +6,52 @@ import withCurrentDate, {
   WithCurrentDateProps
 } from '../../decorators/withCurrentDate';
 
+interface CurrentDateState {
+  date: string;
+}
+
 const today = new Date().toISOString().split('T')[0];
 
-const handleChangeDate = ({
-  target: { value }
-}: React.ChangeEvent<HTMLInputElement>) => {
-  Routes.Router.pushRoute(`/map/${value.replace(/-/g, '/')}`); // Value will be a date in yyyy-mm-dd format
-};
+class CurrentDate extends React.PureComponent<
+  WithCurrentDateProps,
+  CurrentDateState
+> {
+  static getDerivedStateFromProps({ currentDate }: WithCurrentDateProps) {
+    return {
+      date: currentDate.toISOString().split('T')[0]
+    };
+  }
 
-const CurrentDate: React.SFC<WithCurrentDateProps> = ({ currentDate }) => (
-  <Card fluid={true}>
-    <Card.Content>
-      <Card.Meta>Current year</Card.Meta>
-      <Card.Description textAlign="center">
-        <input
-          max={today}
-          min="01-01-0000"
-          onChange={handleChangeDate}
-          required
-          type="date"
-          value={currentDate.toISOString().split('T')[0]}
-        />
-      </Card.Description>
-    </Card.Content>
-  </Card>
-);
+  handleChangeDate = ({
+    target: { value }
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    if (value) {
+      Routes.Router.pushRoute(`/map/${value.replace(/-/g, '/')}`); // Value will be a date in yyyy-mm-dd format
+      return;
+    }
+    this.setState({ date: value });
+  };
+
+  render() {
+    const { date } = this.state;
+    return (
+      <Card fluid={true}>
+        <Card.Content>
+          <Card.Meta>Current year</Card.Meta>
+          <Card.Description textAlign="center">
+            <input
+              max={today}
+              min="01-01-0000"
+              onChange={this.handleChangeDate}
+              required
+              type="date"
+              value={date}
+            />
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
 export default withCurrentDate(CurrentDate);
