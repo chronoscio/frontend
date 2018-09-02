1cdb5ae8ad0b09949e381571faa3bdc35b5a3d6f
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-02 18:26:48 +0200

    Fix typescript bug

--- packages/app/components/MainMap/decorators/territoriesToGeojson.ts
+++ packages/app/components/MainMap/decorators/territoriesToGeojson.ts
@@ -1,10 +1,11 @@
 import { withProps } from 'recompose';
-import { featureCollection, FeatureCollection } from '@turf/helpers';
+import { featureCollection } from '@turf/helpers';
+import { SourceOptionData } from 'react-mapbox-gl/lib/util/types'; // This type is very similar to FeatureCollection from @turf/helpers
 
 import { WithFetchTerritoriesProps } from './withFetchTerritories';
 
 export interface TerritoriesToGeojsonProps {
-  geojson: FeatureCollection; // TODO find a good type here
+  geojson: SourceOptionData; // TODO find a good type here
 }
 
 /**
