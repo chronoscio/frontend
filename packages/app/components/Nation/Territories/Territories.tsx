1eda271f9e99443cd669e79fa96cbbef2ddc6de9
whirish <lpoflynn@protonmail.ch>
2018-09-08 16:40:55 -0700

    withTerritoryRequest

--- packages/app/components/Nation/Territories/Territories.tsx
+++ packages/app/components/Nation/Territories/Territories.tsx
@@ -2,50 +2,36 @@ import * as React from 'react';
 import { Icon, List, ListProps } from 'semantic-ui-react';
 import { compose } from 'recompose';
 
-import mockData from '../../mockData';
+import { Territory } from '../../mockData';
 import Routes from '../../../routes';
 import withCurrentDate, {
   WithCurrentDateProps
 } from '../../decorators/withCurrentDate';
 import withEditMode, { WithEditModeProps } from '../../decorators/withEditMode';
+import withTerritoryRequest, {
+  WithTerritoryRequestProps
+} from '../../decorators/withTerritoryRequest';
+
+interface ServerResponse {
+  data: TerritoryData[];
+}
+
+interface TerritoryData {
+  endDate?: Date;
+  geometry: any;
+  id: number;
+  startDate: Date;
+}
 
 const Territories: React.SFC<
-  ListProps & WithCurrentDateProps & WithEditModeProps
-> = ({ currentDate, isEditMode }) => (
-  <List selection={true}>
-    {mockData.map(({ endDate: endDateFromData, id, startDate }) => {
-      // If no endDate is specified, we consider it today
-      const endDate = endDateFromData ? endDateFromData : new Date();
-
-      // Convert `startDate` to yyyy/mm/dd format
-      const url = startDate
-        .toISOString()
-        .split('T')[0]
-        .split('-')
-        .join('/');
-
-      const isActive = currentDate >= startDate && currentDate <= endDate;
-      return (
-        <Routes.Link key={id} route={`/map/${url}`}>
-          <List.Item>
-            <List.Header>
-              {isActive && <Icon name="caret right" />}
-              From {startDate.getFullYear()} to{' '}
-              {endDateFromData ? endDate.getFullYear() : 'today'}
-            </List.Header>
-            {isActive && (
-              <List.Content>
-                Currently {isEditMode ? 'editing' : 'shown on map'}
-              </List.Content>
-            )}
-          </List.Item>
-        </Routes.Link>
-      );
-    })}
-  </List>
-);
+  ListProps &
+    WithCurrentDateProps &
+    WithEditModeProps &
+    WithTerritoryRequestProps
+> = ({ currentDate, isEditMode, data }) => <List selection={true}>{data}</List>;
 
 export default compose(
   withEditMode,
-  withCurrentDate
+  withCurrentDate,
+  withTerritoryRequest
 )(Territories);
