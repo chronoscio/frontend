1a5168026fccb4f2896b7e7af97b78e25bb5fad4
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-18 23:40:21 +0200

    Fix bug territories props

--- packages/app/components/Nation/Territories/Territories.tsx
+++ packages/app/components/Nation/Territories/Territories.tsx
@@ -10,57 +10,64 @@ import withCurrentDate, {
 import withCurrentNation, {
   WithCurrentNationProps
 } from '../../Nation/decorators/withCurrentNation';
-import withFetchTerritories from '../../MainMap/decorators/withFetchTerritories';
+import withFetchTerritories, {
+  WithFetchTerritoriesProps
+} from '../../MainMap/decorators/withFetchTerritories';
 
 const Territories: React.SFC<
-  ListProps & WithCurrentDateProps & WithCurrentNationProps & WithRouterProps
-  > = ({
-    currentDate,
-    currentNation,
-    router: {
-      query: { day, month, year }
-    }
-  }) => (
-      <List selection={true}>
-        {this.props.territories
-          .filter(({ nation }) => nation === currentNation)
-          .map(({ endDate: endDateFromData, id, startDate }) => {
-            // If no endDate is specified, we consider it today
-            const endDate = endDateFromData ? endDateFromData : new Date();
+  ListProps &
+    WithCurrentDateProps &
+    WithCurrentNationProps &
+    WithFetchTerritoriesProps &
+    WithRouterProps
+> = ({
+  currentDate,
+  currentNation,
+  router: {
+    query: { day, month, year }
+  },
+  territories
+}) => (
+  <List selection={true}>
+    {territories
+      .filter(({ nation }) => nation === currentNation)
+      .map(({ endDate: endDateFromData, id, startDate }) => {
+        // If no endDate is specified, we consider it today
+        const endDate = endDateFromData ? endDateFromData : new Date();
 
-            // Convert `startDate` to yyyy/mm/dd format
-            const url = startDate
-              .toISOString()
-              .split('T')[0]
-              .split('-')
-              .join('/');
+        // Convert `startDate` to yyyy/mm/dd format
+        const url = startDate
+          .toISOString()
+          .split('T')[0]
+          .split('-')
+          .join('/');
 
-            const isActive = currentDate >= startDate && currentDate <= endDate;
+        const isActive = currentDate >= startDate && currentDate <= endDate;
 
-            return (
-              <Routes.Link key={id} route={`/map/${url}/${currentNation}`}>
-                <List.Item>
-                  <List.Header>
-                    {isActive && <Icon name="caret right" />}
-                    From {startDate.getFullYear()} to{' '}
-                    {endDateFromData ? endDate.getFullYear() : 'today'}
-                  </List.Header>
-                  {isActive && (
-                    <List.Content>
-                      Currently shown on map.{' '}
-                      <Routes.Link
-                        route={`/map/${year}/${month}/${day}/${currentNation}/edit`}
-                      >
-                        <a>Edit</a>
-                      </Routes.Link>
-                    </List.Content>
-                  )}
-                </List.Item>
-              </Routes.Link>
-            );
-          })}
-      </List>
-    );
+        return (
+          <Routes.Link key={id} route={`/map/${url}/${currentNation}`}>
+            <List.Item>
+              <List.Header>
+                {isActive && <Icon name="caret right" />}
+                From {startDate.getFullYear()} to{' '}
+                {endDateFromData ? endDate.getFullYear() : 'today'}
+              </List.Header>
+              {isActive && (
+                <List.Content>
+                  Currently shown on map.{' '}
+                  <Routes.Link
+                    route={`/map/${year}/${month}/${day}/${currentNation}/edit`}
+                  >
+                    <a>Edit</a>
+                  </Routes.Link>
+                </List.Content>
+              )}
+            </List.Item>
+          </Routes.Link>
+        );
+      })}
+  </List>
+);
 
 export default compose(
   withCurrentDate,
