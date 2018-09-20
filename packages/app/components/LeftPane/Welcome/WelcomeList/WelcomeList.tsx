5721995e24724dec8dab52667aa707528af55365
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-20 23:31:52 +0200

    Add validation

--- packages/app/components/LeftPane/Welcome/WelcomeList/WelcomeList.tsx
+++ packages/app/components/LeftPane/Welcome/WelcomeList/WelcomeList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, List } from 'semantic-ui-react';
+import { List } from 'semantic-ui-react';
 import { compose } from 'recompose';
 import { subscribe } from 'react-contextual';
 
