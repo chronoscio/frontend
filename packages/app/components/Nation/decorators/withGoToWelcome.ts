6b1d57eb0613dbbfa69ec5674d1b3ce1495e95e8
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-22 12:58:57 +0200

    Add a go to homepage back button

---
+++ packages/app/components/Nation/decorators/withGoToWelcome.ts
@@ -0,0 +1,22 @@
+import { compose, withHandlers } from 'recompose';
+
+import Routes from '../../../routes';
+import withCurrentDate, {
+  WithCurrentDateProps
+} from '../../CurrentDate/decorators/withCurrentDate';
+
+export interface WithGoToWelcomeProps {
+  goToWelcome(): void;
+}
+
+/**
+ * Decorator that adds a `goToWelcome` function, to navigate to the welcome
+ * page.
+ */
+export default compose(
+  withCurrentDate,
+  withHandlers<WithCurrentDateProps, WithGoToWelcomeProps>({
+    goToWelcome: ({ currentDateAsUrl }) => () =>
+      Routes.Router.pushRoute(currentDateAsUrl)
+  })
+);
