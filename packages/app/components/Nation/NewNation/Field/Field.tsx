e66984529a1ed2d33c47df6936911efb2637f7d3
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-22 10:59:06 +0200

    Change error popup design

--- packages/app/components/Nation/NewNation/Field/Field.tsx
+++ packages/app/components/Nation/NewNation/Field/Field.tsx
@@ -4,7 +4,7 @@ import {
   FieldProps as RFFFieldProps,
   FieldRenderProps
 } from 'react-final-form';
-import { Form, Input, Popup } from 'semantic-ui-react';
+import { Form, Input, Label } from 'semantic-ui-react';
 
 interface SUIFieldProps {
   as?: string | React.ComponentType;
@@ -16,32 +16,26 @@ interface SUIFieldProps {
  */
 const renderField: React.SFC<SUIFieldProps & FieldRenderProps> = ({
   as: T = Input,
-  children,
   input,
   label,
   meta,
   ...otherProps
-}) => (
-  <Form.Field>
-    {label && <label htmlFor={input.name}>{label}</label>}
-    <Popup
-      content={meta && (meta.error || meta.submitError)}
-      inverted={true}
-      on="click"
-      open={
-        !!meta &&
-        !meta.valid &&
-        (!meta.pristine || meta.touched) &&
-        !meta.dirtySinceLastSubmit
-      }
-      position="top center"
-      size="mini"
-      trigger={<T id={input.name} {...input} {...otherProps} />}
-    />
-
-    {children}
-  </Form.Field>
-);
+}) => {
+  const showError = !!(
+    meta &&
+    meta.touched &&
+    (meta.error || meta.submitError)
+  );
+  return (
+    <Form.Field>
+      {label && <label htmlFor={input.name}>{label}</label>}
+      <T id={input.name} {...input} {...otherProps} />
+      {showError && (
+        <Label pointing={true}>{meta.error || meta.submitError}</Label>
+      )}
+    </Form.Field>
+  );
+};
 
 interface FieldProps extends RFFFieldProps, SUIFieldProps {}
 
